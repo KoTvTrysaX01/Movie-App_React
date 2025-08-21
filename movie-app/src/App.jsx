519395f47cb932ba835eeb656fcd59a21da498b7
Vadim Elshin <elshinvadim@gmail.com>
2025-08-21 23:27:40 +0200

    Fetching movies data - Token is not valid for some reason

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,15 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Search from "./components/Search.jsx";
+import {useState, useEffect} from "react";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method:'GET',
+    headers:{
+        accept: 'application/json',
+        authorization: 'Bearer ' + API_KEY,
+    }
+}
+
+const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTgzNDI3ZTFhNjFjYjczNjllMTlmZjNhNTVjM2M4MCIsIm5iZiI6MTc1NTgwOTgyNi44NzUsInN1YiI6IjY4YTc4ODIyYzNmYTJmM2UwNGZkMmYyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SH517W8YOtQx6YB6ocUel33Cms1npTEPDfCjn_KcRGI'
+    }
+};
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const fetchMovies = async () => {
+        try{
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const response = await fetch(url, options);
+            if(!response.ok){
+                throw new Error();
+            }
+            const data = await response.json();
+            console.log(data);
+        }catch (error) {
+            console.log(error);
+            setErrorMessage('Error fetching movies. Please try again later');
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    }, []);
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline text-red-500">
-                Hello world!
-            </h1>
-        </div>
+        <main>
+            <div className="pattern">
+                <div className="wrapper">
+                    <header>
+                        <img src="./hero.png" alt="Hero Banner" />
+                        <h1>Find <span className="text-gradient">Movies</span>You'll Enjoy Without the Hassle</h1>
+                        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                    </header>
+                    <section className={"all-movies"}>
+                        <h2>All Movies</h2>
+                        {errorMessage && <p className={"text-red-500"}>{errorMessage}</p>}
+                    </section>
+                </div>
+            </div>
+        </main>
     )
 }
 
---
+++ movie-app/src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Search = ({searchTerm, setSearchTerm}) => {
+    return (
+        <div className="search">
+            <div>
+                <img src={"search.svg"} alt={"search"} />
+                <input
+                type={"text"}
+                placeholder={"Search through thousands of movies"}
+                defaultValue={searchTerm}
+                onChange={(e) => searchTerm = e.target.value}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Search;
